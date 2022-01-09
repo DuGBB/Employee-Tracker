d0cf5cf6ebd6e3010b7f9c376e971b80899c149d
Donald Biggers <DuG@tests-Air.attlocal.net>
2022-01-09 13:51:27 -0600

    added employee menu choice

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ function Company() {
 
 }
 
+function dbCall(sql) {
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        } else {
+            console.table(rows);
+        }
+    });
+}
+
 Company.prototype.initializeCompany = function () {
 
     inquirer.prompt([
@@ -16,31 +27,49 @@ Company.prototype.initializeCompany = function () {
             name: 'menu',
             message: 'Choose between the following options:',
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
-            
+
         }
     ]).then(({ menu }) => {
         if (menu === 'View All Departments') {
-            this.departmentView();
+            this.departmentsView();
+        }
+        if (menu === 'View All Roles') {
+            this.partsView();
+        }
+        if (menu === 'View All Employees') {
+            this.employeesView();
         }
     })
-   // db.end();
+    // db.end();
 }
 
-Company.prototype.departmentView = function () {
+Company.prototype.departmentsView = function () {
     const sql = `SELECT id, name FROM departments`;
 
-    db.query(sql, (err, rows) => {
-        if (err) {
-            console.log(err.message);
-            return;
-        } else {
-            console.table(rows);
-        }
-        // res.json({
-        //     message: 'success',
-        //     data: rows
-        // });
-    });
+    dbCall(sql);
+}
+
+Company.prototype.partsView = function () {
+    const sql = `SELECT a.id, a.title, a.salary, b.name
+                 FROM parts a INNER JOIN departments b
+                 ON a.department_id = b.id`;
+
+    dbCall(sql);
+}
+
+Company.prototype.employeesView = function () {
+    const sql = `SELECT a.id, a.first_name, a.last_name,
+                b.title, c.name, b.salary, d.first_name as manager_first_name, 
+                d.last_name as manager_last_name 
+                FROM employees a
+                INNER JOIN parts b
+                ON a.part_id = b.id
+                INNER JOIN departments c
+                ON b.department_id = c.id
+                INNER JOIN employees d
+                ON a.manager_id = d.id`;
+
+                dbCall(sql);
 }
 
 new Company().initializeCompany();
